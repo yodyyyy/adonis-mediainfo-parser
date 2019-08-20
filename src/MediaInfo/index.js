0d662ddba405358c3573920ddb1d58d0bfc63fc6
axlyody <axlyod@gmail.com>
2019-08-20 10:40:16 +0700

    ....

--- src/MediaInfo/index.js
+++ src/MediaInfo/index.js
@@ -10,32 +10,21 @@ class MediaInfo {
    * @returns {JSON}
    * @memberof MediaInfo
    */
-  _getMediaInfo(url) {
+  get(url) {
     return new Promise((resolve, reject) => {
       execFile('mediainfo', ['--output=JSON', url], (err, stdout) => {
         if (err) {
           if (err.code === 'ENOENT') {
+            reject()
             throw new Error('mediainfo not installed')
           }
         } else {
-          resolve(stdout)
+          resolve(JSON.parse(stdout))
         }
       })
     })
   }
 
-  /**
-   * Return the data
-   *
-   * @method getInstance
-   *
-   * @param {*} file
-   * @returns {JSON}
-   * @memberof MediaInfo
-   */
-  async getInstance(file) {
-    return await this._getMediaInfo(file)
-  }
 }
 
 module.exports = MediaInfo
